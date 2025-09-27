dfddaf453d361ad9e1f00ed59fd4db5df734e2e3
Sayed Hossain <sayedhossain.hossaini@nexttoptech.org>
2025-09-27 16:12:51 +0330

    initial commit

---
+++ public/theme/card.ts
@@ -0,0 +1,17 @@
+import {
+  CardDesignTokens,
+  CardTokenSections,
+} from '@primeuix/themes/types/card';
+
+export const root: CardTokenSections.Root = {
+  borderRadius: '0.5rem',
+};
+
+export const body: CardTokenSections.Body = {
+  padding: '1rem',
+};
+
+export default {
+  root,
+  body,
+} satisfies CardDesignTokens;
---
+++ src/app/features/publications/publications.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Card } from 'primeng/card';
+
+@Component({
+  selector: 'app-publications',
+  templateUrl: './publications.html',
+  styleUrl: './publications.scss',
+  imports: [Card],
+})
+export class Publications {}
