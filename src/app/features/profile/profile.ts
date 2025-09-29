de44e39eb5f3a6158d70631b8eaae1d13486435e
Sayed Hossain <sayedhossain.hossaini@nexttoptech.org>
2025-09-29 18:45:15 +0330

    feat(Profile): Implemented profile component.

--- src/app/features/profile/profile.ts
+++ src/app/features/profile/profile.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Card } from 'primeng/card';
+import { Button } from 'primeng/button';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.html',
   styleUrl: './profile.scss',
-  imports: [Card],
+  imports: [Card, Button, NgOptimizedImage],
 })
 export class Profile {}
