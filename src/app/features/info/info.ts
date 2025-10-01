aa6858699aadd5b4e0a4c72593c5e128ca0edb27
Sayed Hossain <sayedhossain.hossaini@nexttoptech.org>
2025-10-01 19:01:37 +0330

    feat(Info): Implemented Bio info component.

--- src/app/features/info/info.ts
+++ src/app/features/info/info.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Card } from 'primeng/card';
+import Bio from './models/bio.model';
+import { PrimeIcons } from 'primeng/api';
 
 @Component({
   selector: 'app-info',
@@ -7,4 +9,32 @@ import { Card } from 'primeng/card';
   styleUrl: './info.scss',
   imports: [Card],
 })
-export class Info {}
+export class Info {
+  bio: Bio[] = [
+    {
+      icon: PrimeIcons.MAP_MARKER,
+      title: 'Based in',
+      value: 'Iran'
+    },
+    {
+      icon: PrimeIcons.GITHUB,
+      title: 'Github',
+      value: 'amirdiffer'
+    },
+    {
+      icon: PrimeIcons.LINKEDIN,
+      title: 'Linkedin',
+      value: 'differofeveryone'
+    },
+    {
+      icon: PrimeIcons.CIRCLE_FILL,
+      title: 'Medium',
+      value: 'differofeveryone'
+    },
+    {
+      icon: PrimeIcons.ENVELOPE,
+      title: 'Email',
+      value: 'differofeveryone@gmail.com'
+    },
+  ];
+}
