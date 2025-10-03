16cb53416000bcb30c4eb2314261ec23eef3e02e
Sayed Hossain <sayedhossain.hossaini@nexttoptech.org>
2025-10-03 23:15:44 +0330

    feat(TechStack): Implemented tech stack component.

--- src/app/features/tech-stack/tech-stack.ts
+++ src/app/features/tech-stack/tech-stack.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Card } from 'primeng/card';
+import { Chip } from 'primeng/chip';
 
 @Component({
   selector: 'app-tech-stack',
   templateUrl: './tech-stack.html',
   styleUrl: './tech-stack.scss',
-  imports: [Card],
+  imports: [Card, Chip],
 })
 export class TechStack {}
